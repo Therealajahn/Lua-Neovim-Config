2669a81f19309746028b5f5592810f5c5597d43e
AJahn Kimble <Therealajahn>
2024-11-01 17:10:48 -0500

    added new binding to switch and stay in zenmode

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
+--Remember hidden buffers
+vim.o.hidden = true
+--Adjusts how long to wait for potential next key of a combo
+vim.o.timeoutlen = 150
 -- Static line numbers
-
 vim.opt.number = true
 -- Set relative line numbers
 vim.wo.relativenumber = true
@@ -28,8 +31,9 @@ vim.keymap.set('n','<Space>j',':w<CR>')
 --Save all buffers
 vim.keymap.set('n','<Space>jk',':wa<CR>')
 --Reset config
-vim.keymap.set('n','<Space>jk',
+vim.keymap.set('n','<Space>jkl',
 	function()
+		vim.cmd('w')
 		vim.cmd('source ' .. vim.env.MYVIMRC)
 		vim.cmd('Lazy sync')
 	end,
@@ -38,6 +42,7 @@ vim.keymap.set('n','<Space>jk',
 
 --Exit current buffer
 vim.keymap.set('n','<Space>h',':q<CR>')
+vim.keymap.set('n','<Space>hn',':q!<CR>')
 
 -- Vertical and Horizontal split
 vim.keymap.set('n','E',':vsp<CR>')
@@ -60,13 +65,14 @@ vim.keymap.set('n','`l','<c-w>L')
 --Tab Stuff
 vim.keymap.set('n','gn',':tabnew<CR>')
 vim.keymap.set('n','gh','gt')
+vim.keymap.set('n','ghj',':ZenMode<CR><CR>gt:ZenMode<CR><CR>')
 vim.keymap.set('n','gy','gT')
 vim.keymap.set('n','ty',':tabclose<CR>')
 --File Navigation toggle
 vim.keymap.set('n','OU',':Oil<CR>')
 vim.keymap.set('n','<c-ou>',':Oil --float<CR>')
 --Toggle Zenmode
-vim.keymap.set('n','qq',':ZenMode<CR>')
+vim.keymap.set('n','qq',':ZenMode<CR><CR>')
 --Toggle Limelight
 vim.keymap.set('n','mm',':Limelight!!<CR>')
 
@@ -75,7 +81,7 @@ vim.api.nvim_create_autocmd("BufRead", {
   pattern = "*",
 	callback = function()
     -- Lua code to run on every file open
-		vim.cmd(':Limelight')
+		vim.cmd(':Limelight!!')
   end,
 })
 --An autocommand for oil
@@ -94,56 +100,25 @@ vim.api.nvim_create_autocmd("BufEnter",{
 
 -- LAZYVIM (plugin manager)
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-end
-
+---- Install lazy.nvim
+--if not vim.loop.fs_stat(lazypath) then
+--  print('Installing lazy.nvim....')
+--  vim.fn.system({
+--    'git',
+--    'clone',
+--    '--filter=blob:none',
+--    'https://github.com',
+--    '--branch=stable', -- latest stable release
+--    lazypath,
+--  })
+--end
 vim.opt.rtp:prepend(lazypath)  -- Prepend the runtime path for lazy.nvim
-
--- Function to load all plugin configuration files from lua/plugins/
-local function load_plugins()
-  local plugins = {}
-
-  -- Get all Lua files in the plugins directory
-  local plugin_files = vim.fn.glob(vim.fn.stdpath("config") .. "/lua/plugins/*.lua", true, true)
-
-  -- Loop through each plugin file and require it
-  for _, file in ipairs(plugin_files) do
-    local plugin_name = file:match("lua/plugins/(.*)%.lua$")
-    local plugin_config = require("plugins." .. plugin_name)
-
-    -- Add plugin configuration to the plugins table
-    vim.list_extend(plugins, plugin_config)
-  end
-
-  return plugins
-end
 
 -- Lazy.nvim setup to install plugins
 
--- Plugin configurations
 require('lazy').setup({
-	load_plugins(),
-  {'folke/tokyonight.nvim'},
-  {
-    'L3MON4D3/LuaSnip',
-    dependencies = {
-      'rafamadriz/friendly-snippets', -- Collection of snippets including React
-    },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
-	{
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  { 'folke/tokyonight.nvim'},
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
 	{ import = 'plugins/oil' },
@@ -153,33 +128,10 @@ require('lazy').setup({
 			window = {
 				width = 90,
 			}
-	}	
+		}	
   },
 	{ 'junegunn/limelight.vim' },
 	{ 'shaunsingh/moonlight.nvim' },
-	{ 'feline-nvim/feline.nvim' },
-	{ 'nvim-tree/nvim-web-devicons' },
-	{
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-},
-{
-    "NStefan002/visual-surround.nvim",
-    config = function()
-        require("visual-surround").setup({
-            -- your config
-        })
-    end,
-    -- or if you don't want to change defaults
-    -- config = true
-},
-{ 'metakirby5/codi.vim' },
 })
 
 
@@ -189,14 +141,12 @@ vim.opt.termguicolors = true
 --vim.cmd.colorscheme('tokyonight')
 -- Apply moonlight theme
 require('moonlight').set()
-require('feline').setup() 
 
 
 
 --Telescope settings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', 'ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
+vim.keymap.set('n', 'fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', 'fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', 'fh', builtin.help_tags, { desc = 'Telescope help tags' })
